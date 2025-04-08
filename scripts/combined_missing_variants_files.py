95882cfd8dea6b8bac8a51df742e2466f82d6940
hafa786 <hafizm.shehzad@gmail.com>
2025-04-08 14:18:50 +0000

    update the script

--- scripts/combined_missing_variants_files.py
+++ scripts/combined_missing_variants_files.py
@@ -1,10 +1,12 @@
 import os
 import csv
-import gzip
+import subprocess
+from tqdm import tqdm
+import time
 
 # file constant
 folder_path = '/mnt/disks/data/data-directory/variant_qc/panel_variant_qc'
-output_fname = folder_path + "/combined_sorted_output_file.tsv.gz"
+output_fname = folder_path + "/combined_sorted_output_file.tsv"
 # Output tsv Gzipped file path
 output_gzipped_tsv = output_fname + ".gz"
 skipped_file_name = 'chrX.removed.add_LCR.add_XPAR.add_failed_filter_awk.tsv'
@@ -19,7 +21,7 @@ def get_files_list():
     return files
 
 
-def main():
+def main_script():
     filenames = get_files_list()
     filenames.sort()
 
@@ -43,19 +45,80 @@ def main():
                 reader = csv.reader(infile, delimiter='\t')
                 for row in reader:
                     if len(row) == 24:
-                        final_header = "\t".join(row)+"\tchrom\tpos\n"
+                        # check if header is none output file
                         if row[0] == 'Variant' and output_file_header is None:
-                                    out_file.write(final_header)
+                            row.insert(0, '#chrom')
+                            row.insert(1, 'pos')
+                            final_header = "\t".join(row)+"\n"
+                            out_file.write(final_header)
                         else:
                             splited_variant = row[0].split(":")
-                            # print(splited_variant)
-                            chromosome_value = splited_variant[0]
-                            position_value = splited_variant[1]
-                            line = "\t".join(row)
-                            out_file.write(f"{line.strip()}\t{chromosome_value}\t{position_value}\n")
+                            row.insert(0, splited_variant[0])
+                            row.insert(1, splited_variant[1])
+                            line = f"\t".join(row)+"\n"
+                            out_file.write(line)
                     else:
                         print(f"Sorry! The file {fname} does not consist on proper csv columns.")
                         exit(1)
             print(f"Finished!")
 
-main()
+# convert tsv to gz using bgzip
+def convert_tsv_to_gz(input_file, output_file):
+    # Get the total size of the input file for progress calculation
+    file_size = os.path.getsize(input_file)
+    
+    # Open the input file for reading and the output file for writing
+    with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
+        # Start bgzip process
+        process = subprocess.Popen(
+            ['bgzip', '-c', input_file],  # '-c' to write to stdout
+            stdout=outfile,
+            stderr=subprocess.PIPE
+        )
+        
+        # Use tqdm to display progress based on file size
+        with tqdm(total=file_size, unit='B', unit_scale=True, desc="Compressing") as pbar:
+            # Read in chunks and update progress
+            while True:
+                chunk = infile.read(1024 * 1024)  # Read 1MB chunks
+                if not chunk:
+                    break
+                outfile.write(chunk)
+                pbar.update(len(chunk))
+            
+            # Wait for bgzip to finish
+            process.wait()
+
+        # # Check the process return code and handle accordingly
+        if process.returncode == 0:
+            print(f"Process succeeded with output: {output_gzipped_tsv}")
+        else:
+            print(f"Process failed with error: {process.returncode}")
+
+
+
+# convert bgzip to tbi
+def convert_gz_to_tbi():
+    output_gz = output_gzipped_tsv
+    # Step 1: Recompress the file using bgzip (ensure it is in BGZF format)
+    bgzip_command = ['bgzip', '-c', output_gz]
+    # output_gz = output_gzipped_tsv
+    
+    with open(output_gz, 'wb') as output_f:
+        subprocess.run(bgzip_command, stdout=output_f)
+
+    # Step 2: Index the BGZF file using tabix
+    tabix_command = ['tabix', '-s', '1', '-b', '2', '-e', '2', output_gz]
+    
+    # Run the tabix command to generate the index (.tbi)
+    subprocess.run(tabix_command)
+
+    print(f"Index file created: {output_gz}.tbi")
+
+
+main_script()
+# convert_tsv_to_gz(output_fname, output_gzipped_tsv)
+# convert_gz_to_tbi()
+
+
+
