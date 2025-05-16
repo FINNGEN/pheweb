e44091c8a53c27f282e92d73252997d5037d74ce
hafa786 <hafizm.shehzad@gmail.com>
2025-05-16 12:11:29 +0000

    remove the self

--- pheweb/serve/encoder.py
+++ pheweb/serve/encoder.py
@@ -1,15 +1,15 @@
 
-from flask.json.provider import DefaultJSONProvider
+from flask.json.provider import DefaultJSONProvider # type: ignore
 
 class FGJSONEncoder(DefaultJSONProvider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
 
-    def default(self,o):
+    def default(o):
         try:
             rep =o.json_rep()
         except Exception as e:
             pass
         else:
             return rep
-        return DefaultJSONProvider.default(self, o)
+        return DefaultJSONProvider.default(o)
