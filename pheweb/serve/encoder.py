3f8be65e069ab609bb95a682adad4a2c3327e046
hafa786 <hafizm.shehzad@gmail.com>
2025-05-20 10:45:01 +0000

    handle error in missing variant

--- pheweb/serve/encoder.py
+++ pheweb/serve/encoder.py
@@ -1,15 +1,14 @@
-
 from flask.json.provider import DefaultJSONProvider # type: ignore
 
 class FGJSONEncoder(DefaultJSONProvider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
 
-    def default(o):
+    def default(self,o):
         try:
             rep =o.json_rep()
         except Exception as e:
             pass
         else:
             return rep
-        return DefaultJSONProvider.default(o)
+        return DefaultJSONProvider.default(self, o)
