f09cca229ea60831317b26590f0ef04b496d8c36
hafa786 <hafizm.shehzad@gmail.com>
2025-05-21 14:26:32 +0300

    Simplify the QC variant imports issue#570 (#573)
    Updated the QC variant data import script
    * use argparse
    * separate header processing from handling rows
    * change from positional columns to use a mapping form columns to columns for different X chromosome file
    * make chromosomes numeric + remove leading chr

--- scripts/import_missing_qc_variants_files.py
+++ scripts/import_missing_qc_variants_files.py
@@ -1,5 +1,6 @@
-import os, sys
+import os
 import csv
+import argparse
 
 def get_files_list(input_path, output_path):
     files = []
@@ -8,6 +9,56 @@ def get_files_list(input_path, output_path):
         if output_path not in file_path and file_path.endswith('.tsv'):
             files.append(file_path)
     return files
+
+# mapping the columns name from chrom xfile => autosomal
+COLUMNS_MAPPING_DICT = {
+    "Variant": "Variant",
+    "callRate": "variant_qc.call_rate",
+    "AC": "variant_qc.AC",
+    "AF": "variant_qc.AF",
+    "nCalled": "variant_qc.n_called",
+    "nNotCalled": "variant_qc.n_not_called",
+    "nHet": "variant_qc.n_het",
+    "dpMean": "variant_qc.dp_stats.mean",
+    "dpStDev": "variant_qc.dp_stats.stdev",
+    "gqMean": "variant_qc.gq_stats.mean",
+    "gqStDev": "variant_qc.gq_stats.stdev",
+    "nNonRef": "variant_qc.n_non_ref",
+    "pHWE": "variant_qc.p_value_hwe",
+    "FILTERS": "filters",
+    "QD": "info.QD",
+    "IS_LCR": "IS_LCR",
+    "failed_filter": "failed_filter"
+}
+
+def generate_resulted_row(output_header, row, xfile_header):
+    """
+    generate resulted row after using dict mapping
+    """
+    resulted_row = []
+    xfile_columns_index = {}
+    splited_output_header = output_header.split("\t")
+    
+    for index,name in enumerate(xfile_header): 
+        xfile_columns_index[name] = index
+
+    variant = row[xfile_columns_index['Variant']].replace('chr', '').replace('X', '23')
+    splited_variant = variant.split(":")
+    resulted_row.append(splited_variant[0])
+    resulted_row.append(splited_variant[1])
+
+    # skip #chrom and pos from output_header
+    for index,column_name in enumerate(splited_output_header[2:]):
+        if column_name in COLUMNS_MAPPING_DICT:
+            column_name_xfile = COLUMNS_MAPPING_DICT[column_name]
+            if 'Variant' in column_name:
+                resulted_row.append(variant)
+            else:
+                resulted_row.append(row[xfile_columns_index[column_name_xfile]])
+        else:
+            resulted_row.append('NA')
+
+    return f"\t".join(resulted_row)+"\n"
 
 def main_script(input_path, output_path):
     filenames = get_files_list(input_path, output_path)
@@ -21,83 +72,54 @@ def main_script(input_path, output_path):
     # read in the files
     with open(output_path,"wt",encoding="utf-8") as out_file:
         try:
-            # output file header
-            output_file_reader = csv.reader(out_file)
+            # flag to check header is written or not
             is_header_written = False
-            # store the header for output file
-            output_file_header = next(output_file_reader) if not os.path.getsize(output_path) == 0 else None
+            output_header = []
             for fname in filenames:
                 with open(fname,"rt",encoding="utf-8") as infile:
                     print(fname)
                     print('Loading data ...')
                     reader = csv.reader(infile, delimiter='\t')
+                    header = next(reader)
+                    header_row = "#chrom\tpos\t" + "\t".join(header)
+                    if is_header_written is False:
+                        output_header = header_row
+                        out_file.write(header_row +'\n')
+                        is_header_written = True
                     for row in reader:
+                        # autosomal file
                         if len(row) == 24:
-                            # check if header is not present in output file
-                            if row[0] == 'Variant' and is_header_written is False and output_file_header is None:
-                                row.insert(0, '#chrom')
-                                row.insert(1, 'pos')
-                                final_header = "\t".join(row)+"\n"
-                                out_file.write(final_header)
-                                is_header_written = True
-                            elif row[0] == 'Variant' and is_header_written is True and output_file_header is None:
-                                continue
-                            else:
-                                splited_variant = row[0].split(":")
-                                row.insert(0, splited_variant[0])
-                                row.insert(1, splited_variant[1])
-                                line = f"\t".join(row)+"\n"
-                                out_file.write(line)
-                        elif len(row) == 29:
-                            if (row[25] == 'Variant'):
-                                continue
-                            else:
-                                new_row = []
-                                splited_variant = row[25].split(":")
-                                new_row.insert(0, splited_variant[0])
-                                new_row.insert(1, splited_variant[1])
-                                new_row.insert(2, row[25])
-                                new_row.insert(3, row[15])
-                                new_row.insert(4, row[11])
-                                new_row.insert(5, row[12])
-                                new_row.insert(6, row[16])
-                                new_row.insert(7, row[17])
-                                new_row.insert(8, 'NA')
-                                new_row.insert(9, row[19])
-                                new_row.insert(10, 'NA')
-                                new_row.insert(11, row[3])
-                                new_row.insert(12, row[4])
-                                new_row.insert(13, row[7])
-                                new_row.insert(14, row[8])
-                                new_row.insert(15, row[20])
-                                new_row.insert(16, 'NA')
-                                new_row.insert(17, 'NA')
-                                new_row.insert(18, 'NA')
-                                new_row.insert(19, row[22])
-                                new_row.insert(20, row[2])
-                                new_row.insert(21, row[24])
-                                new_row.insert(22, 'NA')
-                                new_row.insert(23, 'NA')
-                                new_row.insert(24, row[26])
-                                new_row.insert(25, row[28])
-                                line = f"\t".join(new_row)+"\n"
-                                out_file.write(line)
+                            variant = row[0].replace('chr', '')
+                            splited_variant = variant.split(":")
+                            row.insert(0, splited_variant[0])
+                            row.insert(1, splited_variant[1])
+                            row[2] = variant
+                            line = f"\t".join(row)+"\n"
+                            out_file.write(line)
+                        # Xfile check
+                        elif len(row) > 24:
+                            line = generate_resulted_row(output_header, row, header)
+                            out_file.write(line)
                         else:
                             print(f"Sorry! The file {fname} and {row} does not consist on proper tsv columns.")
                             exit(1)
                 print(f"Finished!")
         except Exception as e:  # Catch any exception
             print(f"An error occurred: {e}")
+            raise
 
 # check the argument 
-if len(sys.argv) == 3:
-    INPUT_FOLDER_PATH = sys.argv[1]
-    if f"/{sys.argv[2]}".endswith('.tsv'):
-        OUTPUT_FOLDER_PATH = INPUT_FOLDER_PATH + f"/{sys.argv[2]}"
-        main_script(INPUT_FOLDER_PATH, OUTPUT_FOLDER_PATH)
-    else:
-        print("Sorry, output file name should be tsv. e-g string.tsv")
-        exit(1)
+parser = argparse.ArgumentParser(description="The QC variant import script!")
+
+parser.add_argument('input_file_path', type=str)
+parser.add_argument('output_file_name', type=str)
+
+args = parser.parse_args()
+
+INPUT_FOLDER_PATH = args.input_file_path
+if f"/{args.output_file_name}".endswith('.tsv'):
+    OUTPUT_FOLDER_PATH = INPUT_FOLDER_PATH + f"/{args.output_file_name}"
+    main_script(INPUT_FOLDER_PATH, OUTPUT_FOLDER_PATH)
 else:
-    print("Sorry, qc variants folder path or output tsv file name is missing!")
+    print("Sorry, output file name should be tsv. e-g string.tsv")
     exit(1)
