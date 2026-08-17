e43d6d0b7eec9fdb076921a84a156843e19e2798
Paavilainen <topi.paavilainen@helsinki.fi>
2026-08-17 14:27:15 +0300

    frontend adjustments for new drugs

--- ui/src/common/commonTableColumn.tsx
+++ ui/src/common/commonTableColumn.tsx
@@ -1181,7 +1181,7 @@ const phenotypeColumns = {
     },
     disease: {
       Header: () => (<span style={{ textDecoration: "underline" }}>disease</span>),
-      accessor: "disease",
+      accessor: "diseaseName",
       filterMethod: (filter, row) => row[filter.id] <= filter.value,
       Cell: textCellFormatter
     },
@@ -1196,7 +1196,7 @@ const phenotypeColumns = {
       accessor: "drugId",
       filterMethod: (filter, row) => row[filter.id] <= filter.value,
       Cell: props => props.value === "NA" ? props.value : (
-        <a href={`https://www.ebi.ac.uk/chembl/g/#search_results/all/query=${props.value}`}
+        <a href={`https://www.ebi.ac.uk/chembl/explore/compound/${props.value}`}
            target="_blank" rel="noopener noreferrer">
           {props.value}
         </a>
